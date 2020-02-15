805d3265f52e9c22fa4e6562cde71595056c978e
sergioeabarcaf <sergioaf1991@gmail.com>
2020-02-15 12:50:51 -0300

    feat(articleService): create service

--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -1,43 +1,70 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface articleGetInterface {
+  title: String;
+  story_title: String;
+  author: String;
+  created_at: String;
+  objectID: String;
+  url: String;
+  story_url: String;
+}
+
+export interface articleinterface {
+  title: String;
+  author: String;
+  created_at: String;
+  objectID: String;
+  url: String;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 
-
 export class ArticlesService {
   
-  url = 'http://localhost:8000/articles';
-  articles = [];
+  url = 'http://localhost:8080/articles';
+  articles: articleinterface[];
 
   constructor(public http: HttpClient) { }
 
   async getArticles() {
-    return this.http.get(this.url).toPromise()
-      .then((listArticles: any[]) => {
-        this.articles = listArticles;
-        console.log(this.articles);
-        return true;
-      })
-      .catch((err) => {
-        console.error(err);
-        return err;
-      });
+    try {
+      this.articles = [];
+      const listArticles = await this.http.get<articleGetInterface[]>(this.url).toPromise();
+
+      const articles: articleinterface[] = listArticles.reduce((acc, article: articleGetInterface) => {
+        if (!article.story_title && !article.title) {
+          return acc;
+        }
+
+        acc.push({
+          title: article.story_title ? article.story_title : article.title,
+          url: article.story_url ? article.story_url : article.url,
+          author: article.author,
+          created_at: article.created_at,
+          objectID: article.objectID
+        });
+        return acc;
+      }, []);
+
+      this.articles = articles;
+      return true;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
   };
 
-  async delete(id: string) {
+  delete(id: string) {
     return this.http.delete(`${this.url}/${id}`).toPromise()
-      .then((newListArticles: any[]) => {
-        this.articles = [];
-        this.articles = newListArticles;
-        console.log(this.articles);
-        return true;
+      .then((resp) => {
+        this.getArticles(); 
       })
       .catch((err) => {
         console.error(err);
-        return err;
       });
-
   };
 }
